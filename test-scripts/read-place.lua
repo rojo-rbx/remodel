a0e34f16e8391699fb54744d3cb0e76d84648386
Lucien Greathouse <me@lpghatguy.com>
2019-09-15 18:26:24 -0700

    Upgrade tests to assert about parents

--- test-scripts/read-place.lua
+++ test-scripts/read-place.lua
@@ -1,5 +1,6 @@
 local game = remodel.readPlaceFile("test-models/place-with-models.rbxlx")
 
+assert(game.Parent == nil)
 assert(game.ClassName == "DataModel")
 
 assert(game.ReplicatedStorage.Name == "ReplicatedStorage")
