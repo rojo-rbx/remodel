efce88223da34536709c8c044b526184381ec8b4
Lucien Greathouse <me@lpghatguy.com>
2019-09-23 23:17:39 -0700

    Update parent test script

--- test-scripts/parent.lua
+++ test-scripts/parent.lua
@@ -1,8 +1,6 @@
 local root = remodel.readModelFile("test-models/folder-and-value.rbxmx")[1]
 assert(root.Name == "Root")
-
--- TODO: Models are currently parented to a DataModel silently still
--- assert(root.Parent == nil)
+assert(root.Parent == nil)
 
 local child1 = root:GetChildren()[1]
 assert(child1 ~= nil)
