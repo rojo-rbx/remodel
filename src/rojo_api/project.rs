2ddcc1e9c9a2d838ba8f48df5c61f7a22c78b6da
Lucien Greathouse <me@lpghatguy.com>
2020-01-02 17:11:44 -0800

    First stab at properties in Rojo projects

--- src/rojo_api/project.rs
+++ src/rojo_api/project.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 use librojo::project::{Project, ProjectNode};
+use rbx_dom_weak::{AmbiguousRbxValue, RbxValue, UnresolvedRbxValue};
 use rlua::{Context, FromLua, Value};
 
 pub struct RemodelProject(pub Project);
@@ -58,7 +59,7 @@ impl<'lua> FromLua<'lua> for RemodelProjectNode {
         let mut class_name = None;
         let mut ignore_unknown_instances = None;
         let mut children = BTreeMap::new();
-        let properties = HashMap::new();
+        let mut properties: HashMap<String, RemodelUnresolvedValue> = HashMap::new();
 
         for pair in lua_table.pairs::<String, Value<'_>>() {
             let (key, value) = pair?;
@@ -70,7 +71,7 @@ impl<'lua> FromLua<'lua> for RemodelProjectNode {
                     ignore_unknown_instances = Some(bool::from_lua(value, context)?)
                 }
                 "$properties" => {
-                    return Err(rlua::Error::external("$properties is not implemented yet"));
+                    properties = HashMap::from_lua(value, context)?;
                 }
                 _ => {
                     children.insert(key, RemodelProjectNode::from_lua(value, context)?.0);
@@ -79,6 +80,12 @@ impl<'lua> FromLua<'lua> for RemodelProjectNode {
         }
 
         let path = path.map(|value| PathBuf::from(value));
+
+        // Unwrap all of our Remodel values into values that Rojo understands.
+        let properties = properties
+            .into_iter()
+            .map(|(key, value)| (key, value.0))
+            .collect();
 
         let project_node = ProjectNode {
             path,
@@ -89,5 +96,90 @@ impl<'lua> FromLua<'lua> for RemodelProjectNode {
         };
 
         Ok(RemodelProjectNode(project_node))
+    }
+}
+
+struct RemodelUnresolvedValue(UnresolvedRbxValue);
+
+impl<'lua> FromLua<'lua> for RemodelUnresolvedValue {
+    fn from_lua(lua_value: Value<'lua>, context: Context<'lua>) -> rlua::Result<Self> {
+        let value = lua_to_unresolved_value(lua_value, context)?;
+        Ok(RemodelUnresolvedValue(value))
+    }
+}
+
+fn lua_to_unresolved_value<'lua>(
+    lua_value: Value<'lua>,
+    context: Context<'lua>,
+) -> rlua::Result<UnresolvedRbxValue> {
+    match lua_value {
+        Value::String(value) => Ok(UnresolvedRbxValue::Ambiguous(AmbiguousRbxValue::String(
+            value.to_str()?.to_owned(),
+        ))),
+        Value::Boolean(value) => Ok(UnresolvedRbxValue::Concrete(RbxValue::Bool { value })),
+        Value::Number(num) => Ok(UnresolvedRbxValue::Ambiguous(AmbiguousRbxValue::Float1(
+            num,
+        ))),
+        Value::Integer(num) => Ok(UnresolvedRbxValue::Ambiguous(AmbiguousRbxValue::Float1(
+            num as f64,
+        ))),
+        Value::Table(table) => {
+            let ty = table.raw_get("Type")?;
+            let value = table.raw_get("Value")?;
+
+            match (ty, value) {
+                (Value::Nil, Value::Nil) => {
+                    // When neither field is defined, we assume this is an
+                    // array-like table specifying an ambiguous value.
+
+                    let values = Vec::<f64>::from_lua(Value::Table(table), context)?;
+
+                    match values.as_slice() {
+                        [a, b] => Ok(UnresolvedRbxValue::Ambiguous(AmbiguousRbxValue::Float2(
+                            *a, *b,
+                        ))),
+                        [a, b, c] => Ok(UnresolvedRbxValue::Ambiguous(AmbiguousRbxValue::Float3(
+                            *a, *b, *c,
+                        ))),
+                        [a, b, c, d] => Ok(UnresolvedRbxValue::Concrete(RbxValue::UDim2 {
+                            value: (*a as f32, *b as i32, *c as f32, *d as i32),
+                        })),
+                        [a, b, c, d, e, f, g, h, i, j, k, l] => {
+                            Ok(UnresolvedRbxValue::Concrete(RbxValue::CFrame {
+                                value: [
+                                    *a as f32, *b as f32, *c as f32, *d as f32, *e as f32,
+                                    *f as f32, *g as f32, *h as f32, *i as f32, *j as f32,
+                                    *k as f32, *l as f32,
+                                ],
+                            }))
+                        }
+                        _ => {
+                            return Err(rlua::Error::FromLuaConversionError {
+                                from: "not a Roblox value",
+                                to: "Roblox value",
+                                message: None,
+                            });
+                        }
+                    }
+                }
+                (Value::String(_ty_name), _not_nil) => {
+                    unimplemented!();
+                }
+                _ => {
+                    return Err(rlua::Error::FromLuaConversionError {
+                        from: "not a Roblox value",
+                        to: "Roblox value",
+                        message: None,
+                    });
+                }
+            }
+        }
+        _ => {
+            return Err(rlua::Error::FromLuaConversionError {
+                from: "not a Roblox value",
+                to: "Roblox value",
+                message: None,
+            });
+        }
     }
 }
