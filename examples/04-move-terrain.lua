3776678ee161dc33c7588f82a00995830d462a25
Ryan C. Scott <ryan@5pmcasual.com>
2021-05-17 21:39:18 -0700

    Resolved a bad indexing in the move-terrain example script

--- examples/04-move-terrain.lua
+++ examples/04-move-terrain.lua
@@ -3,7 +3,7 @@ local inputGame = remodel.readPlaceFile("input-place.rbxlx")
 local outputGame = remodel.readPlaceFile("output-place.rbxlx");
 
 -- This isn't possible inside Roblox, but works just fine in Remodel!
-outputGame.Terrain:Destroy()
-inputGame.Terrain.Parent = outputGame
+outputGame.Workspace.Terrain:Destroy()
+inputGame.Workspace.Terrain.Parent = outputGame
 
 remodel.writePlaceFile(outputGame, "output-place-updated.rbxlx")
