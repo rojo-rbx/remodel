b67b81066701ae6675e06e7d0244e6cf18cd942d
qualadore@qualadore.com <me@qualadore.com>
2022-07-02 13:40:37 -0500

    Format

--- src/remodel_api/remodel.rs
+++ src/remodel_api/remodel.rs
@@ -573,7 +573,7 @@ impl UserData for Remodel {
             let meta = fs::metadata(path).map_err(mlua::Error::external)?;
             Ok(meta.is_dir())
         });
-        
+
         methods.add_function("removeFile", |_context, path: String| {
             fs::remove_file(path).map_err(mlua::Error::external)
         });
@@ -581,6 +581,5 @@ impl UserData for Remodel {
         methods.add_function("removeDir", |_context, path: String| {
             fs::remove_dir_all(path).map_err(mlua::Error::external)
         });
-
     }
 }
